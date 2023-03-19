efb324758995d04e001ffca25cbdfab52d7ea045
Abdullah Ibne Hanif Arean <70502574+AbdullahArean@users.noreply.github.com>
2023-03-19 11:25:32 +0600

    to run in windows

---
+++ testwindows.py
@@ -0,0 +1,7 @@
+import os
+os.system('start cmd.exe @cmd /k "python Lsr.py configA.txt"')
+os.system('start cmd.exe @cmd /k "python Lsr.py configB.txt"')
+os.system('start cmd.exe @cmd /k "python Lsr.py configC.txt"')
+os.system('start cmd.exe @cmd /k "python Lsr.py configD.txt"')
+os.system('start cmd.exe @cmd /k "python Lsr.py configE.txt"')
+os.system('start cmd.exe @cmd /k "python Lsr.py configF.txt"')
